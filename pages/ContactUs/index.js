770468920630b4ddf82e9b9a4258f36d2e0749bb
Thitipong <thitipongn64@nu.ac.th>
2022-09-08 16:57:47 +0700

    my changes

---
+++ pages/ContactUs/index.js
@@ -0,0 +1,15 @@
+import React, { useRef } from 'react';
+
+const ContactUs = () => {
+  return (
+    <div className='container-ContactUs'>
+      <div className='ContactUs'>
+        <h1>สั่งซื้อสำเร็จ</h1>
+        <img src="https://cf.shopee.co.th/file/8f9bfb59c857440e58650f3903573bee" alt="" />
+        <span>ขอบคุณที่ใช้บริการ</span>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs
